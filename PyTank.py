575dd75e7a3f1e5ad938e104080ca55770a8cf16
Andrew Groeneveldt <andrew@andrews-macbook-pro.local>
2018-11-22 21:59:39 -0700

    implemented sensor functions my position enemy position my heading turret direction clear shot - not yet implemented ** bug fixes on maze generation and logical storage **

--- PyTank.py
+++ PyTank.py
@@ -12,6 +12,7 @@ all_sprites = pygame.sprite.LayeredUpdates()
 green_shots = pygame.sprite.Group()
 orange_shots = pygame.sprite.Group()
 enviro_sprites = pygame.sprite.Group()
+maze_map = []
 
 
 def load_image(filename,x=None,y=None):
@@ -48,7 +49,11 @@ class Wall(pygame.sprite.Sprite):
         self.image = load_image('wall.png',200,20)
         self.image = pygame.transform.rotate(self.image,orientation)
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.get_rect(topleft=position)
+        if orientation == 0: self.rect = self.image.get_rect(topleft = (position[0],position[1]-10))
+        if orientation == 90: self.rect = self.image.get_rect(topleft = (position[0]-10,position[1]))
+        if orientation == 180: self.rect = self.image.get_rect(bottomright = (position[0],position[1]+10))
+        if orientation == 270: self.rect = self.image.get_rect(bottomright = (position[0]+10,position[1]))
+
 
 
 class Shot(pygame.sprite.Sprite):
@@ -63,26 +68,26 @@ class Shot(pygame.sprite.Sprite):
         self.radius = 6
         self.heading = heading
         self.image = rotate_ip(self, self.heading.angle_to(pygame.math.Vector2(0, -1)))
-        self.velocity = heading
-        self.rect.move_ip(self.velocity)
+        self.rect.move_ip(self.heading)
     
     def update(self):
         """ update shot position """
-        self.rect.move_ip(self.velocity)
+        self.rect.move_ip(self.heading)
         if pygame.sprite.spritecollideany(self, enviro_sprites) or not self.area.contains(self.rect): self.kill()
 
 
 class Turret(pygame.sprite.Sprite):
     """Class to represent gun turrets"""
 
-    def __init__(self,tank,colour):
+    def __init__(self,tank,colour,heading):
         pygame.sprite.Sprite.__init__(self)
         all_sprites.add(self, layer = 1)
         image_file = colour + 'Turret.png'
         self.base_image = load_image(image_file,50,50)
         self.image = self.base_image
         self.rect = self.image.get_rect(center=tank.rect.center)
-        self.heading = pygame.math.Vector2(0, -20)
+        self.heading = heading * 2
+        if colour == 'green': self.image = rotate_ip(self, self.heading.angle_to(pygame.math.Vector2(0, -1)))
 
 
 class Tank(pygame.sprite.Sprite):
@@ -92,28 +97,36 @@ class Tank(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         if colour != 'green' and colour != 'orange': colour = 'green'
         all_sprites.add(self, layer = 0)
-        if colour == 'green':
-            self.control = green_control
-            self.shot_group = green_shots
-        else:
-            self.control = orange_control
-            self.shot_group = orange_shots
         image_file = colour + 'Tank.png'
         self.base_image = load_image(image_file,75,75)
         self.image = self.base_image
-        self.mask = pygame.mask.from_surface(self.image)
-        self.area = pygame.display.get_surface().get_rect()
         self.rect = self.image.get_rect(center=position)
+        if colour == 'green':
+            self.control = green_control
+            self.shot_group = green_shots
+            self.heading = pygame.math.Vector2(0, 10)
+            self.image = rotate_ip(self, self.heading.angle_to(pygame.math.Vector2(0, -1)))
+        else:
+            self.control = orange_control
+            self.shot_group = orange_shots
+            self.heading = pygame.math.Vector2(0, -10)
+        self.mask = pygame.mask.from_surface(self.image)
         self.radius = 35
-        self.velocity = pygame.math.Vector2()
-        self.heading = pygame.math.Vector2(0, -10)
-        self.turret = Turret(self,colour)
+        self.turret = Turret(self,colour,self.heading)
         self.cooldown = 0
         self.moved = False
         self.turned = False
         self.rotated = False
         self.fired = False
         
+    def get_walls(self):
+        if self.rect.center[0] < 0 or self.rect.center[0] >= 1200:
+            return maze_map[24]
+        if self.rect.center[1] < 0 or self.rect.center[1] >= 800:
+            return maze_map[25]
+        return maze_map[6 * (self.rect.center[1]//200) + self.rect.center[0]//200]
+
+
     def set_enemy(self, tank):
         self.enemy = tank
     
@@ -121,15 +134,30 @@ class Tank(pygame.sprite.Sprite):
         self.turret.kill()
         pygame.sprite.Sprite.kill(self)
     
+    def my_position(self):
+        return self.rect.center
+    
+    def enemy_position(self):
+        return self.enemy.rect.center
+    
+    def my_heading(self):
+        return (360-((round(int(self.heading.angle_to(pygame.math.Vector2(0,-1)))<<1,-1))>>1))%360
+    
+    def turret_direction(self):
+        return (360-((round(int(self.turret.heading.angle_to(pygame.math.Vector2(0,-1)))<<1,-1))>>1))%360
+
+    def clear_shot(self):
+        return False
+    
     def forward(self):
         if self.moved: return False
         self.moved = True
-        self.rect.move_ip(self.heading * 0.75)
+        self.rect.move_ip(self.heading * 0.8)
         if pygame.sprite.collide_mask(self, self.enemy):
-            self.rect.move_ip(-self.heading * 0.75)
+            self.rect.move_ip(-self.heading * 0.8)
             return False
         if pygame.sprite.spritecollideany(self, enviro_sprites, collided = pygame.sprite.collide_mask):
-            self.rect.move_ip(-self.heading * 0.75)
+            self.rect.move_ip(-self.heading * 0.8)
             return False
         return True
     
@@ -178,12 +206,12 @@ class Tank(pygame.sprite.Sprite):
 
         # wrap screen
         if self.rect.x + self.rect.width < 0:
-            self.rect.x = self.area.width
+            self.rect.x = 1200
         if self.rect.y + self.rect.height < 0:
-            self.rect.y = self.area.height
-        if self.rect.x > self.area.width:
+            self.rect.y = 800
+        if self.rect.x > 1200:
             self.rect.x = -self.rect.width
-        if self.rect.y > self.area.height:
+        if self.rect.y > 800:
             self.rect.y = -self.rect.height
 
         # move turret with tank
@@ -198,26 +226,60 @@ class Tank(pygame.sprite.Sprite):
 
 
 def set_up_walls():
-    Wall((0,0),0)
-    Wall((200,0),0)
-    Wall((400,0),0)
-    Wall((800,0),0)
-    Wall((1000,0),0)
-    Wall((0,780),0)
-    Wall((200,780),0)
-    Wall((400,780),0)
-    Wall((800,780),0)
-    Wall((1000,780),0)
-    Wall((0,0),90)
-    Wall((0,400),90)
-    Wall((0,600),90)
-    Wall((1180,0),90)
-    Wall((1180,400),90)
-    Wall((1180,600),90)
-    for y in range(190,700,200):
-        for x in range(190,1100,200):
+    for _ in range(24): maze_map.append({'n':0,'s':0,'e':0,'w':0})
+    maze_map.append({'n':1,'s':1,'e':0,'w':0})
+    maze_map.append({'n':0,'s':0,'e':1,'w':1})
+    Wall((0,10),0)
+    maze_map[0]['n'] = 1
+    Wall((200,10),0)
+    maze_map[1]['n'] = 1
+    Wall((400,10),0)
+    maze_map[2]['n'] = 1
+    Wall((800,10),0)
+    maze_map[4]['n'] = 1
+    Wall((1000,10),0)
+    maze_map[5]['n'] = 1
+    Wall((0,790),0)
+    maze_map[18]['s'] = 1
+    Wall((200,790),0)
+    maze_map[19]['s'] = 1
+    Wall((400,790),0)
+    maze_map[20]['s'] = 1
+    Wall((800,790),0)
+    maze_map[22]['s'] = 1
+    Wall((1000,790),0)
+    maze_map[23]['s'] = 1
+    Wall((10,0),90)
+    maze_map[0]['w'] = 1
+    Wall((10,400),90)
+    maze_map[12]['w'] = 1
+    Wall((10,600),90)
+    maze_map[18]['w'] = 1
+    Wall((1190,0),90)
+    maze_map[5]['e'] = 1
+    Wall((1190,400),90)
+    maze_map[17]['e'] = 1
+    Wall((1190,600),90)
+    maze_map[23]['e'] = 1
+    point = [0,1,2,3,4,6,7,8,9,10,12,13,14,15,16]
+    index = 0
+    for y in range(200,700,200):
+        for x in range(200,1100,200):
             r = random.choice([0,90,180,270])
+            if r == 0:
+                maze_map[point[index]+1]['s'] = 1
+                maze_map[point[index]+7]['n'] = 1
+            elif r == 90:
+                maze_map[point[index]+6]['e'] = 1
+                maze_map[point[index]+7]['w'] = 1
+            elif r == 180:
+                maze_map[point[index]]['s'] = 1
+                maze_map[point[index]+6]['n'] = 1
+            elif r == 270:
+                maze_map[point[index]]['e'] = 1
+                maze_map[point[index]+1]['w'] = 1
             Wall((x,y),r)
+            index += 1
 
 def main():
     """run the game"""
--- orange_control.py
+++ orange_control.py
@@ -16,3 +16,12 @@ def action(my_tank):
     if key[K_k] and my_tank.cooldown == 0:
         my_tank.fire()
 
+    if key[K_RETURN]:
+        walls = my_tank.get_walls()
+        if walls['n']: print('wall to north ',end='')
+        if walls['s']: print('wall to south ',end='')
+        if walls['e']: print('wall to east ',end='')
+        if walls['w']: print('wall to west ',end='')
+        print()
+    if key[K_RSHIFT]:
+        print(my_tank.turret_direction())
